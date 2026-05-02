484f7e05712951488388e43c981d11a51be2f21d
usupovbulat8125 <usupovbulat8125@yandex.ru>
2026-05-02 09:53:32 +0300

    Fix: query_5 и query_6 с from data import (для Jobs)

--- task_query_5/main.py
+++ task_query_5/main.py
@@ -1,5 +1,8 @@
+from data import db_session
+from data.jobs import Jobs
+
 db_name = input()
-global_init(db_name)
-db_sess = create_session()
+db_session.global_init(db_name)
+db_sess = db_session.create_session()
 for job in db_sess.query(Jobs).filter(Jobs.work_size < 20, not Jobs.is_finished):
     print(job)
