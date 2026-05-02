b3d7d8be52a6cc3623f95547503fc84c332ec070
usupovbulat8125 <usupovbulat8125@yandex.ru>
2026-05-02 18:22:20 +0300

    Fix: query_7 добавил импорты и чтение двух БД

--- task_query_7/main.py
+++ task_query_7/main.py
@@ -1,9 +1,17 @@
-db_name = input()
-global_init(db_name)
+from db_session import global_init, create_session
+from users import User
+
+db_name_1 = input()
+db_name_2 = input()
+
+global_init(db_name_1)
 db_sess = create_session()
 db_sess.query(User).filter(User.address == "module_1", User.age < 21).update(
     {"address": "module_3"}
 )
 db_sess.commit()
+
+global_init(db_name_2)
+db_sess = create_session()
 for user in db_sess.query(User).filter(User.address == "module_3"):
     print(user)
