03bf7ae613b508adc870495b7354e0c2ac0c25cb
usupovbulat8125 <usupovbulat8125@yandex.ru>
2026-05-02 09:18:28 +0300

    Fix: убрал относительный импорт и методы из users.py

--- task_martian_model/users.py
+++ task_martian_model/users.py
@@ -1,7 +1,5 @@
 import datetime
 import sqlalchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from .db_session import SqlAlchemyBase
 
 
 class User(SqlAlchemyBase):
@@ -19,9 +17,3 @@ class User(SqlAlchemyBase):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                       default=datetime.datetime.now)
-
-    def set_password(self, password):
-        self.hashed_password = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.hashed_password, password)
