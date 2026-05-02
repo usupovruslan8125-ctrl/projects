216dac11e8997e8c0e910bc2516e9cd3ceba954a
usupovbulat8125 <usupovbulat8125@yandex.ru>
2026-05-02 10:01:41 +0300

    Fix: query_5 ~Jobs.is_finished (PEP8-safe)

--- task_query_5/main.py
+++ task_query_5/main.py
@@ -4,5 +4,5 @@ from jobs import Jobs
 db_name = input()
 global_init(db_name)
 db_sess = create_session()
-for job in db_sess.query(Jobs).filter(Jobs.work_size < 20, Jobs.is_finished != True):
+for job in db_sess.query(Jobs).filter(Jobs.work_size < 20, ~Jobs.is_finished):
     print(job)
